89775fb340fb9f52e70604e4f4551eea9cee2ea7
sodium-sys <73413664+sodium-sys@users.noreply.github.com>
2025-11-21 16:07:06 +0800

    Update slack_place.m

--- utils/slack_place.m
+++ utils/slack_place.m
@@ -1,40 +1,55 @@
-function [K,nu] = slack_place(Sigma,p,T)
-    
+function [K, nu] = slack_place(Sigma, p, T)
+% SLACK_PLACE - Stabilize observer via iterative pole placement
+% Attempts to stabilize the functional observer by iteratively adjusting
+% the null space selection for pole placement with desired pole constraints.
+
+% Args:
+% Sigma -- observer coefficient matrix
+% p -- regression matrix used for null space computation
+% T -- normalization factor for singular value calculation
+
+% Outputs:
+% K -- stabilization gain matrix
+% nu -- selected null space basis used for stabilization
+
+% Author: Ziyuan Luo
+% Email: ziyuan.luo@bit.edu.cn
+% Last revision: Nov.18, 2025
+%------------- BEGIN CODE --------------
+
+    % Initialize parameters and extract observer dynamics
     thresh = 1e-20;
     Sigmazp = Sigma(:, end-size(Sigma,1)+1:end);
-    svd_p= svd(p*p'/T);
+    svd_p = svd(p * p' / T);
     
+    % Generate desired poles: keep stable poles, replace unstable ones
     e = eig(Sigmazp);
     idx = abs(e) > 1;
     desired_poles = e;
-    desired_poles(idx) = .8*rand(sum(idx),1) + 0.1;
+    desired_poles(idx) = .8 * rand(sum(idx), 1) + 0.1;
     
-    % svd_trun_index=find(svd_p>thresh); % BA
-    % svd_trun_index=find(svd_p>tol); % auto
-    % svd_trun_index=find(svd_p>5e-9); % ER
-    % svd_trun_index=find(svd_p>5e-8); % Example
-    [U1,~,~] = svd(p);
-    % nu=U1(:,max(svd_trun_index)+1:end)';
-    % nuk = nu(:, end-size(Sigmazp,1)+1:end);
-    warning('off','all');
-    for i = max(find(svd_p>thresh)) :-2:1
-        nu = U1(:,i+1:end)';
+    % Iterative pole placement with varying null space dimensions
+    [U1, ~, ~] = svd(p);
+    warning('off', 'all');
+    
+    for i = max(find(svd_p > thresh)) : -2 : 1
+        nu = U1(:, i+1:end)';
         nuk = nu(:, end-size(Sigmazp,1)+1:end);
+        
+        % Attempt pole placement with current null space
         try 
-            prec=[];
+            prec = [];
             [K, prec] = place(Sigmazp', nuk', desired_poles);
         catch ME
         end
+        
+        % Exit if successful pole placement achieved
         if prec > 0
             break
         end
     end
-    warning('on','all');
-    % error = max(max(abs(K'*nu*p)));
-    % KK = place(Sigmazp', nuk', desired_poles);
-    % rank_svd(ctrb(Sigmazp', nuk'))
-    % test = Sigma - KK'*nu;
-    % max(abs(eig(test(:, end-size(Sigma,1)+1:end))))
-    % max(max(abs(nu*p)))
-    % max(max(abs(KK'*nu*p)))
+    
+    warning('on', 'all');
+
+%------------- END OF CODE --------------
 end
